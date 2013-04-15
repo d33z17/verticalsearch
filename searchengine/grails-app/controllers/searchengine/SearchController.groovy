dc5b8c0bb57fd0863173e85c5085a904bdfc68e6
Derek Sheh <mr.sheh@gmail.com>
2013-04-14 18:10:28 -0700

    refined solr queries, results, and snippets

--- searchengine/grails-app/controllers/searchengine/SearchController.groovy
+++ searchengine/grails-app/controllers/searchengine/SearchController.groovy
@@ -22,21 +22,17 @@ class SearchController {
 		uQ = params.address
 		uQ = uQ.replaceAll("'",'?')							 // replace apostrophes with ?
 		solrparams.setQuery(uQ)
+		solrparams.set("qf","""
+										professor
+										position
+										courses
+										education
+										country
+										""")										 // searchable fields
 		solrparams.set("q.op", "OR")						 // allow in-exact matches
+//		solrparams.set("mm",2)									 // minimum match 2 elements since in-exact
 		solrparams.set("defType", "edismax")		 // set solr to run as edismax
-		solrparams.set("rows",11)								 // set number results returned
-																						 // multiple field query and boost
-		solrparams.set("qf", """\
-													id^1.0
-													professor^20.0
-													website^15.0
-													position^15.0
-													schools^10.0
-													degrees^5.0 
-													country^0.3 
-													education^5.0 
-													courses^1.0
-													""")
+		
 		response = solr.query(solrparams)		
 
 //		render response	// debug
@@ -48,7 +44,7 @@ class SearchController {
 			render "Sorry, I could not find any matches for " + uQ + "<br /><br />"
 
 		/* iterate each matched result in the result list */
-		def profEduc = new Object[doclist.getNumFound()][]
+		def profEduc = [doclist.getNumFound()][]
 		render profEduc
 		for (org.apache.solr.common.SolrDocument doc : doclist) {
 			
@@ -61,18 +57,25 @@ class SearchController {
 			def schools = doc.getFieldValues("schools")
 			
 			profEduc[id][i] = name
+			render profEduc[id][1]
 			
 			// professor name and profile website			
 			render "<span class='name'><a href='" + link + "'>" + name + "</a></span>"			
 			
 			// professor position if exists	
 			if (position != null)
-				render ", " + position
+				render ", is an SFU " + position + " who"
 			
 			// courses taught if any
-			courses.collect {
-				if (!it.isEmpty())
-					render "<br />" + it
+			courses.each {
+				if (!it.isEmpty()) {
+					if (it == courses.first())
+						render " teaches: "
+					else if (it == courses.last())
+					  render "and " + it + "."
+					else
+						render it + ", "
+				}
 			}
 			
 			// schools attended
@@ -82,30 +85,26 @@ class SearchController {
 			def uniName
 			schools.each {
 				
-				if (!it.isEmpty()) {
+				def schoolparams = new org.apache.solr.client.solrj.SolrQuery()
+				def cut = it.replaceAll('Computer','')
+				cut = cut.replaceAll('Canada','')
+				cut = cut.replaceAll('Science','')
+				
+				if (cut.matches('.*\\w.*')) {
+					render "<br /><a href='http://'>" + cut + "</a>"
 					count ++
 				}
 				
-				render "<br /><a href='http://'>" + it + "</a>"
-
-				def schoolparams = new org.apache.solr.client.solrj.SolrQuery()
-				schoolparams.clear()
-				def cut = it.replaceAll('of','')
 				schoolparams.setQuery(cut)
 				schoolparams.set("q.op", "AND")
 				schoolparams.set("defType", "edismax")
-				schoolparams.set("mm",3)
-				schoolparams.set("qf", """\
-				 													schools^20.0
-																	uniRank^10.0
-																	uniName^15.0
-																	""")																	
+				schoolparams.set("qf", "uniName")
 				schoollist = solr.query(schoolparams).getResults()
 				for (org.apache.solr.common.SolrDocument school : schoollist) {
 					
 					uniName = school.getFieldValues("uniName")
 					
-					if (uniName != null) {
+					if (!uniName.isEmpty()) {
 						render ", world comp sci ranking: " + school.getFieldValue("uniRank")
 						totalRank += school.getFieldValue("uniRank")
 					}
@@ -114,12 +113,11 @@ class SearchController {
 			render "<br />" + name + "'s comp sci ranking is: " + Math.round(totalRank/count)
 				
 			doc.getFieldValues("education").collect {
-				if (!it.isEmpty())
+				if (!it.matches("\\S+"))
 					render "<br />" + it
 			}
 			
-			render "<br />"
-			render schoollist
+			render "<br /><br />"
 
 		} // end for (doc : doclist)
 	}
